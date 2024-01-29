f8a7894d4cd6e1a2a555e8f33bb7b7b1c71e5eca
Camille Fourmaintraux <123071999+CamilleFourmaintraux@users.noreply.github.com>
2024-01-29 15:46:45 +0100

    AJout des images pour les items d'armures

---
+++ src/main/java/net/grotacam/tutorialmod/item/custom/ModArmorItem.java
@@ -0,0 +1,13 @@
+package net.grotacam.tutorialmod.item.custom;
+
+import net.minecraft.item.ArmorItem;
+import net.minecraft.item.ArmorMaterial;
+
+public class ModArmorItem extends ArmorItem {
+
+
+
+    public ModArmorItem(ArmorMaterial material, Type type, Settings settings) {
+        super(material, type, settings);
+    }
+}
